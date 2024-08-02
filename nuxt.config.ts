0e49d6cf54a9b9b24bc489772820890e8191ec6b
marcaureldev <ahouandjinoumarcaurel10@gmail.com>
2024-08-02 21:21:51 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  buildModules: [
-    '@nuxtjs/tailwindcss',
-  ],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -13,7 +10,8 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-07-29',
   modules: [
-    '@primevue/nuxt-module'
+    '@primevue/nuxt-module',
+    '@nuxtjs/tailwindcss',
   ],
   primevue: {
     /* Configuration */
